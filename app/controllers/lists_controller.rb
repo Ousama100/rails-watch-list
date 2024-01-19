5d5a6c2b69aed10d0946c2859aa0c8bd416540a2
Oussama Diyab <oussama.diyab@gmail.com>
2024-01-19 10:40:21 +0100

    watchlist

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,10 @@
+class ListsController < ApplicationController
+  def index
+  end
+
+  def shownew
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'lists/index'
+  get 'lists/shownew'
+  get 'lists/create'
+    resources :lists, only: [:index, :show, :new, :create] do
+    resources :movies, only: [:index, :show, :new, :create, :destroy,]
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
